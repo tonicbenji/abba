0fcc44c7c9d87457bf9f55c0a4fb8d43967ad694
Benjamin Glitsos <bengyup@gmail.com>
2019-09-05 16:23:56 +1000

    nothing

--- page-gen/gen-config.js
+++ page-gen/gen-config.js
@@ -4,7 +4,7 @@ module.exports = {
     outputLocation: "public",
     briefLogs: false,
     genSuburbs: true,
-    subset: 0.10,
+    subset: 1,
     business: {
         name: "ABBA",
         trade: "business broker"
